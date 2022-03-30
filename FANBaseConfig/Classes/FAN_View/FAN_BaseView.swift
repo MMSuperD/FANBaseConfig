5fb2549a10e49f067f86954b297be013b9254652
MMSuperD <shdjxywd@sina.com>
2022-03-31 07:37:39 +0800

    更新文件夹的名字

---
+++ FANBaseConfig/Classes/FAN_View/FAN_BaseView.swift
@@ -0,0 +1,52 @@
+//
+//  FAN_BaseView.swift
+//  FANBaseConfig
+//
+//  Created by 王丹 on 2022/3/31.
+//
+
+import UIKit
+
+class FAN_BaseView: UIView {
+    
+    var isDidAddChildView: Bool = false
+    
+    var dataObject: AnyObject?
+    
+
+    public override init(frame: CGRect){
+        super.init(frame: frame)
+
+        self.public_fun1()
+    }
+
+    public required init?(coder: NSCoder){
+        super.init(coder: coder)
+
+        self.public_fun1()
+    }
+    
+    convenience init(frame: CGRect,dataObject: AnyObject = ["mmd":"mmd"] as AnyObject) {
+        self.init(frame: frame)
+        self.dataObject = dataObject
+    }
+    
+    
+    /// 这个方法是为了解决值调用一次  addChildView 方法
+    /// - Returns: Void
+    func public_fun1() -> Void {
+        
+        if !isDidAddChildView {
+            self.addChildView()
+            self.isDidAddChildView = true
+        }
+    }
+    
+    
+    /// 添加子空间
+    /// - Returns: Void
+    open func addChildView() -> Void {
+        
+    }
+
+}
